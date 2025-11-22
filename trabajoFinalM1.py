bdb6a1c7a5492cea9f83b809c204b595d5a0b3c5
Am01102007 <161891643+Am01102007@users.noreply.github.com>
2025-11-21 22:09:49 -0500

    Refactor inventory management system code

--- trabajoFinalM1.py
+++ trabajoFinalM1.py
@@ -1,13 +1,4 @@
-#Sistema de Registro de Productos en Inventario
-# Crear una aplicación modular que permita gestionar el inventario básico de una tienda pequeña.
-#Funcionalidades requeridas:
-
-#1.Registrar productos.
-#2.Mostrar todos los productos registrados.
-#3.Buscar producto por nombre.
-#4.Calcular valor total del inventario.
-#5.Validar entradas y salidas.
-
+#Sistema de inventario
 # Definimos la lista de productos
 productos = []
 #1.Registrar productos.
@@ -99,4 +90,5 @@ def main():
     menu()
 
 if __name__ == "__main__":
+
     main()
